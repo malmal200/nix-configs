d8504a4cfa5dca64d352d82f847decada7914a48
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-10-10 10:33:30 +0100

    Add missing dependency

--- services/mysql.nix
+++ services/mysql.nix
@@ -3,7 +3,8 @@ let
     version = "8.0.16-7";
     package = pkgs.mysql80.overrideAttrs (oldAttrs: {
         name = "percona-server80";
-        meta.version = version;
+        buildInputs = oldAttrs.buildInputs ++ [ rapidjson ];
+        patches = [];
         src = pkgs.fetchurl {
           url = "https://www.percona.com/downloads/Percona-Server-LATEST/Percona-Server-${version}/source/tarball/percona-server-${version}.tar.gz";
           sha256 = "1677jm271l8jy7566r7lb5z1bfbfrc50yfkvggs58w4i4df6i3wg";
